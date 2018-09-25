c7ef62d46ff38f7a78c349f969176a186053eccb
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-09-25 11:50:55 +0200

    App with loop over and restart pop up when questions end

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
+    let allQuestion = QuestionBank()
+    var answerPicked: Bool = false
+    var questionCounter: Int  = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -21,11 +24,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        nextQuestion()
+        
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        
+        if sender.tag == 1 {
+            
+            answerPicked = true
+            
+        } else if sender.tag == 2 {
+            
+            answerPicked = false
+            
+        }
+        
+        checkAnswer()
+        
+        questionCounter += 1
+        
+        nextQuestion()
+        
     }
     
     
@@ -36,15 +57,48 @@ class ViewController: UIViewController {
 
     func nextQuestion() {
         
+        if questionCounter <= 12 {
+            
+            let nextQuestion = allQuestion.list[questionCounter]
+            questionLabel.text = nextQuestion.questionText
+            
+        } else {
+            
+            let alert = UIAlertController(title: "Awesome", message: "You've finished all the questions, do you want to start over?", preferredStyle: .alert)
+            
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+            
+            alert.addAction(restartAction)
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
     }
     
     
     func checkAnswer() {
+            
+        let questionAnswer = allQuestion.list[questionCounter].answer
+        if answerPicked == questionAnswer {
+            
+            print("Nice Done")
+            
+        } else {
+            
+            print("Wrong!")
+            
+        }
         
     }
     
     
     func startOver() {
+        
+        questionCounter = 0
+        nextQuestion()
        
     }
     
